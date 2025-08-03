432d33b53a6716e33fe52bb275da89d96f9a85f2
VictorMarri <victorpujoni@gmail.com>
2025-08-03 13:30:52 -0300

    Maps learning added

--- 10 - Maps/maps.go
+++ 10 - Maps/maps.go
@@ -3,13 +3,37 @@ package main
 import "fmt"
 
 func main() {
-
-	//Maps sao rigidos. Uma vez que voce declarou o tipo de dado que vai conter nos maps, vc so pode trabalhar com esse tipo e nenhum outro diferente
-	//         map[tipoDasChaves]tipoDosValores{}
+	// Exemplo de map simples
 	usuario := map[string]string{
-		"nome:":     "Victor",
+		"nome":      "Victor",
 		"sobrenome": "Marri",
 	}
 
-	fmt.Println(usuario["nome"])
+	fmt.Println("Nome do usuário:", usuario["nome"])
+
+	// Exemplo de map aninhado, simulando um registro mais complexo
+	usuario2 := map[string]map[string]string{
+		"pessoa": {
+			"nome":      "Jane",
+			"sobrenome": "Doe",
+		},
+		"curso": {
+			"nome":   "Sistemas de Informação",
+			"campus": "Barreiro",
+		},
+	}
+
+	fmt.Println("Nome da pessoa:", usuario2["pessoa"]["nome"])
+	fmt.Println("Sobrenome da pessoa:", usuario2["pessoa"]["sobrenome"])
+
+	// Deletando uma chave do map
+	fmt.Println("Antes de deletar 'pessoa':", usuario2)
+	delete(usuario2, "pessoa")
+	fmt.Println("Depois de deletar 'pessoa':", usuario2)
+
+	// Adicionando uma nova chave ao map
+	usuario2["cpf"] = map[string]string{
+		"cpf": "12345678978",
+	}
+	fmt.Println("Após adicionar 'cpf':", usuario2)
 }
